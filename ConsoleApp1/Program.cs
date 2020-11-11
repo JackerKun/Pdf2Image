fed2a6d3a0b66709ada8fbaf233dcd56aa9c89f4
Vitor Correia <vitormiguelcorreia@gmail.com>
2020-11-11 20:47:50 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "c:\\tmp\\Comprovativo.pdf";
+            var t = Pdf2Image.PdfSplitter.GetImages(file, Pdf2Image.PdfSplitter.Scale.High);
+
+            Pdf2Image.PdfSplitter.WriteImages(file, "c:\\tmp", Pdf2Image.PdfSplitter.Scale.High, Pdf2Image.PdfSplitter.CompressionLevel.Medium);
+        }
+    }
+}
